e20f874bc97c758b79cf00130f22a4a254bd684e
Riad <felihriad5@gmail.com>
2021-07-17 20:23:08 +0200

    first commit

---
+++ js-json/checkboxes.js
@@ -0,0 +1,38 @@
+const contract_list = document
+.getElementById("contract-checkboxes");
+const role_list = document.
+getElementById("role-checkboxes");
+const level_list = document
+.getElementById("level-checkboxes");
+const location_list = document
+.getElementById("location-checkboxes");
+const language_list = document
+.getElementById("language_checkboxes");
+const tools_list = document
+.getElementById("tools-checkboxes");
+
+
+function creat_checkboxe(content)
+{
+    return `
+    <input id="${content}-id" 
+    class="filter-ui__ckeckbox" type="checkbox" />
+    <label for="${content}-id">${content}</label>`;
+}
+function fill_list(list_element,array)
+{
+    array.forEach(function(item)
+    {
+        const list_item = document.createElement("li");
+        list_item.classList.add("filter-ui__item");
+        list_item.classList.add("checkbox");
+        list_item.innerHTML = creat_checkboxe(item);
+        list_element.append(list_item);
+    });
+}
+fill_list(contract_list,contracts);
+fill_list(role_list,roles);
+fill_list(level_list,levels);
+fill_list(location_list,locations);
+fill_list(language_list,languages);
+fill_list(tools_list,tools);
---
+++ js-json/reduce.js
@@ -0,0 +1,50 @@
+const json_object;
+fetch("data.json").then(function(response)
+{
+    return response.json();
+}).then(function(data)
+{
+    json_object = data;
+}).catch(function(error)
+{
+    console.log(error.message);
+});
+
+
+//fucntion description:
+//for example if the 1st user object of the array (the json array)
+// khow PHP and the 2nd knows python and 3rd PHP ect...
+//so the return of the function id ["PHP","python",...]
+//each element of the retured array is unique 
+// this allow us to put all the languages in one array in none repeatable way
+
+function set_all_types(property_name,typeof_property)
+{
+    
+    return json_object.reduce(function(object,acc)
+    {
+        let array = object[property_name];
+        //converting string to array in such case
+        if(typeof_property === String)
+        {
+            array = array.split(" ");
+        }
+        
+        array.forEach(function(item)
+        {
+            if(!acc.contains(item))
+            {
+                acc.add(item);
+            }
+        });
+
+       return acc;
+    },[]);
+}
+
+const roles = set_all_types("role",String);
+const levels  = set_all_types("level",String);
+const contracts = set_all_types("contract",String);
+const locations = set_all_types("location",String);
+const languages = set_all_types("languages",Array);
+const tools = set_all_types("tools",Array);
